4c1b913fd2e8ee2c9f8d01037998a2a61f6ee5b5
bt3762334-commits <bt3762334@gmail.com>
2026-06-04 02:18:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -106,3 +106,34 @@ document.querySelector(".contact-form").addEventListener("submit", (e) => {
 
   }, 1000);
 });
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+
+});
+const cursor = document.querySelector(".cursor-glow");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
