c0873b2fe5ab0e33f09258c537f346b51307a610
bt3762334-commits <bt3762334@gmail.com>
2026-06-04 02:11:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -77,3 +77,15 @@ document.querySelectorAll(".project-card").forEach(card => {
   });
 
 });
+// SMOOTH SCROLL
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const target = document.querySelector(link.getAttribute("href"));
+
+    target.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
